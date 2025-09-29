4c0126224129f59428a2477348f1d2d56d76a7b5
Kerry N. Wood <kerry.wood@asterism.ai>
2025-09-29 19:40:09 +0000

    added an example to pull rest directly

--- via_rest.py
+++ via_rest.py
@@ -1,8 +1,9 @@
 import requests
+import reformat
 
 if __name__ == "__main__":
     _URL = "https://datacenter.iers.org/data/9/finals2000A.all"
     data = requests.get( _URL, verify=False )
     lines = data.text.split('\n')
-    new_lines = reformatLines( lines )
-    print(new_lines)
+    new_lines = reformat.reformatLines( lines )
+    print('\n'.join(new_lines))
